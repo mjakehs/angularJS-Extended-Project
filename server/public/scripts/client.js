1f3838a349390f01aa2f4bdf80f41fc86522a6de
Jake Marzitelli <mjakehs@gmail.com>
2018-09-26 17:41:23 -0500

    create http post

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,23 @@
 const app = angular.module('TrackerApp', []);
 
-app.controller('TrackerController', ['$http', function($http){
+app.controller('EntryController', ['$http', function($http){
     let vm = this;
     vm.message = 'Hi there!';
 
+    vm.submitEntry = function(entry) {
+        console.log(entry);
+        $http({
+            method: 'POST',
+            url: '/entries',
+            data: entry
+        }).then(function(response){
+            console.log(response);
+        }).catch(function(error){
+            alert('Error in entry post!');
+        })
+    }//end entry post
+
+    vm.getEntries = function(){
+        console.log('Placeholder for get entries');
+    }
 }])
