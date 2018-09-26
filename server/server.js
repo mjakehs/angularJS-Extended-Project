547bc24d9aca4117a53f972771e7b28808ed724a
Jake Marzitelli <mjakehs@gmail.com>
2018-09-26 17:10:33 -0500

    add server, serve static, add vendors

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('server/public'));
+
+app.listen(PORT, () => {
+    console.log('App is listening on port: ', PORT);
+})
