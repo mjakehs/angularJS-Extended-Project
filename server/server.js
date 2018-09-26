8882ccfd499f2f00ff1ccd034e1e1c4409242f9a
Jake Marzitelli <mjakehs@gmail.com>
2018-09-26 17:48:12 -0500

    add and configure bodyParser, app uses entries route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static('server/public'));
+app.use('/entries', require('./routes/entries.router'));
 
 app.listen(PORT, () => {
     console.log('App is listening on port: ', PORT);
