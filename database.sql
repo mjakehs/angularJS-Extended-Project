0553ad5877a2050aa5f3c03207d2dc22e5d25b80
Jake Marzitelli <mjakehs@gmail.com>
2018-09-26 19:52:43 -0500

    add ajax get for all entries

--- database.sql
+++ database.sql
@@ -3,10 +3,10 @@ CREATE DATABASE "time_tracker";
 CREATE TABLE "entry" (
     "id" SERIAL PRIMARY KEY,
     "entry" VARCHAR(255) NOT NULL,
-    "project_id" REFERENCES "project", 
+    "project_id" INT REFERENCES "project", 
     "entry_date" DATE NOT NULL,
-    "start_time" DATE NOT NULL,
-    "end_time" DATE NOT NULL
+    "start_time" BIGINT NOT NULL,
+    "end_time" BIGINT NOT NULL
 );
 
 CREATE TABLE "project" (
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,7 @@ const app = angular.module('TrackerApp', []);
 
 app.controller('EntryController', ['$http', function ($http) {
     let vm = this;
-    vm.message = 'Hi there!';
+    vm.entries = [];
 
     vm.submitEntry = function (entry) {
         vm.formatDateObject(entry);
@@ -20,8 +20,16 @@ app.controller('EntryController', ['$http', function ($http) {
 
     vm.getEntries = function () {
         console.log('Placeholder for get entries');
+        $http.get('/entries')
+        .then( function(response){
+            console.log(response.data);
+            vm.entries = response.data;
+        }).catch(function(error){
+            alert('Error in entry post!');
+        })
     }
-
+    /** Stores project start and end times as milliseconds from 1970
+     * So that time comparison can be more dynamic  **/
     vm.formatDateObject = function (entry) {
         let start = entry.startTime;
         let end = entry.endTime;
@@ -34,6 +42,7 @@ app.controller('EntryController', ['$http', function ($http) {
             start.getHours(),
             start.getMinutes()
         )
+        //conditional allows users to rollover to next day if work extends overnight
         if (end.getTime() < start.getTime()) {
             endDate = new Date(
                 (entryDate.getYear() + 1900),
@@ -54,5 +63,7 @@ app.controller('EntryController', ['$http', function ($http) {
         }
         vm.newEntry.startTime = startDate.getTime();
         vm.newEntry.endTime = endDate.getTime();
-    }
+    }//end formatDateObject
+
+    vm.getEntries();
 }])
