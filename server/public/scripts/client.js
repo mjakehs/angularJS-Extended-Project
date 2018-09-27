bb5fc02250f9f8ba0866945630a49cdcb9fe18d0
Jake Marzitelli <mjakehs@gmail.com>
2018-09-26 20:53:35 -0500

    add ngRoute + routeprovider to config

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,22 @@
-const app = angular.module('TrackerApp', []);
+const app = angular.module('TrackerApp', ['$ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: './views/home.html'
+    }) .when('/entry', {
+        templateUrl: './views/entry.html',
+        controller: 'EntryController as vm'
+    }).when('/project', {
+        templateUrl: './views/project.html',
+        controller: 'ProjectController as vm'
+    }).when('/report', {
+        templateUrl: './views/report.html',
+        controller: 'ReportController as vm'
+    }).otherwise({
+        template: '<h1>404 Error</h1>'
+    })
+}])
+
 
 app.controller('EntryController', ['$http', function ($http) {
     let vm = this;
