016013be4a43725d74651b468ca3611680c51658
Jake Marzitelli <mjakehs@gmail.com>
2018-09-28 11:07:18 -0500

    add auth middleware function

---
+++ server/modules/authentication-middleware.js
@@ -0,0 +1,11 @@
+//Middleware function that runs on auth protected routes to make sure user
+//is logged in.
+const rejectUnauthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+}
+
+module.exports = rejectUnauthenticated;
