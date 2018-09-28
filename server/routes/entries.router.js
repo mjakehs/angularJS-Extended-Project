c29b5f0bacbe68b459eb168c905d30dbaefd214e
Jake Marzitelli <mjakehs@gmail.com>
2018-09-28 13:11:33 -0500

    require auth middleware in entry/project routes

--- server/routes/entries.router.js
+++ server/routes/entries.router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 router.post('/', rejectUnauthenticated, (req, res) => {
     pool.query(`INSERT INTO "entry" ("entry", "project_id", "entry_date","hours")
--- server/routes/projects.router.js
+++ server/routes/projects.router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 router.post('/', rejectUnauthenticated, (req, res) => {
     console.log(req.body);
