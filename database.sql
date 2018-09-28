1c4dc4f407b6f9650c6e7f0f60bcbc2543a2e76d
Jake Marzitelli <mjakehs@gmail.com>
2018-09-28 15:40:23 -0500

    create sql table statements for messages, connections, project associations

--- database.sql
+++ database.sql
@@ -20,3 +20,26 @@ CREATE TABLE "person" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "connections" (
+	"id" SERIAL PRIMARY KEY,
+	"person_one_id" INT,
+	"person_two_id" INT,
+	"connection_date" DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE "messages" (
+	"id" SERIAL PRIMARY KEY,
+	"message_body" VARCHAR(300) NOT NULL,
+	"from_person_id" INT NOT NULL,
+	"to_person_id" INT NOT NULL,
+	"message_date" DATE NOT NULL
+);
+
+CREATE TABLE "person_project" (
+	"id" SERIAL PRIMARY KEY,
+	"project_id" INT REFERENCES "project",
+	"owner_id" INT,
+	"member_id" INT,
+	"addition_date" DATE DEFAULT CURRENT_DATE
+);
