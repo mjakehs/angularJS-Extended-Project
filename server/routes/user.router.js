b465715621e0d878de134c85e09044f3fcefc1ac
Jake Marzitelli <mjakehs@gmail.com>
2018-09-28 11:20:49 -0500

    link authentication middleware and routes to server

---
+++ server/routes/user.router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const pool = require('../modules/pool');
+const userStrategy = require('../strategies/user.strategy');
+
+const router = express.Router();
+
+// Handles Ajax request for user information if user is authenticated
+router.get('/', rejectUnauthenticated, (req, res) => {
+  res.send(req.user);
+});
+
+router.post('/register', (req, res, next) => {
+  console.log('req: ', req.body);
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
+  const queryText = 'INSERT INTO person (username, password) VALUES ($1, $2) RETURNING id';
+  pool.query(queryText, [username, password])
+    .then(() => { res.sendStatus(201); })
+    .catch((err) => { next(err); });
+});
+
+// Handles login form authenticate/login POST
+router.post('/login', userStrategy.authenticate('local'), (req, res) => {
+  res.sendStatus(200);
+});
+
+// clear all server session information about this user
+router.get('/logout', (req, res) => {
+  // Use passport's built-in method to log out the user
+  req.logout();
+  res.sendStatus(200);
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
+require('dotenv').config();
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Passport Session Configuration //
+app.use(sessionMiddleware);
+// start up passport sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static('server/public'));
+app.use('/api/user', userRouter);
 app.use('/entries', require('./routes/entries.router'));
 app.use('/projects', require('./routes/projects.router'));
+
 
 app.listen(PORT, () => {
     console.log('App is listening on port: ', PORT);
