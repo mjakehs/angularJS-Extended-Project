689e5755ae92bede64b390cf50e55d4740efc22d
Jake Marzitelli <mjakehs@gmail.com>
2018-09-28 15:45:34 -0500

    add routes with empty queries for connections

--- server/routes/connections.router.js
+++ server/routes/connections.router.js
@@ -3,3 +3,36 @@ const pool = require('../modules/pool');
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 //add new connection, delete a connection, get all connections
+
+router.post('/', rejectUnauthenticated, (req, res) => {
+    console.log(req.body);
+    pool.query()
+    .then((results) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error in connections post: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+    pool.query()
+    .then( (results) => {
+        res.send(results.rows);
+    })
+    .catch( (error) => {
+        console.log('Error in connections get: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.delete('/', rejectUnauthenticated, (req, res) => {
+    pool.query()
+    .then( (results) => {
+        res.sendStatus(200);
+    })
+    .catch( (error) => {
+        console.log('Error in connections delete: ', error);
+        res.sendStatus(500);
+    })
+})
