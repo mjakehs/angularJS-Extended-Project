4c61ed87da3cf3e1d1388418f7d7607180564f97
Jake Marzitelli <mjakehs@gmail.com>
2018-09-28 11:15:28 -0500

    add login strategy

---
+++ server/strategies/user.strategy.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const encryptLib = require('../modules/encryption');
+const pool = require('../modules/pool');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    pool.query('SELECT id, username FROM person WHERE id = $1', [id]).then((result) => {
+      // Handle Errors
+      const user = result && result.rows && result.rows[0];
+  
+      if (!user) {
+        // user not found
+        done(null, false, { message: 'Incorrect credentials.' });
+      } else {
+        // user found
+        done(null, user);
+      }
+    }).catch((err) => {
+      console.log('query err ', err);
+      done(err);
+    });
+  });
