f25c4905ed897867d28e848a8c882b307fd8119e
Jake Marzitelli <mjakehs@gmail.com>
2018-09-29 12:45:41 -0500

    add ajax for social controller

--- server/public/scripts/social.controller.js
+++ server/public/scripts/social.controller.js
@@ -1,3 +1,31 @@
 app.controller('SocialController', ['$http', function ($http) {
     let vm = this;
+    vm.friends = [];
+    vm.getFriends = function() {
+        $http({
+            method: 'GET',
+            url: '/connections'
+        })
+        .then( function(response){
+            vm.friends = response.data;
+        })
+        .catch( function(error) {
+            alert('Error in friends GET.');
+        })
+    }
+    vm.getCurrentUser = function(){
+        $http({
+            method: 'GET',
+            url: '/api/user'
+        })
+        .then( function(response){
+            vm.currentUser = response.data.username;
+        })
+        .catch( function(error) {
+            alert('Error in current user GET.');
+        })
+    }
+
+    vm.getCurrentUser();
+    vm.getFriends();
 }]);
