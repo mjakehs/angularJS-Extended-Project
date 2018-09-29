29ea7af76782ad382b2ae6961f5cb0e60a066704
Jake Marzitelli <mjakehs@gmail.com>
2018-09-29 16:44:56 -0500

    add routes for connection requests

--- server/routes/connections.router.js
+++ server/routes/connections.router.js
@@ -41,4 +41,64 @@ router.delete('/', rejectUnauthenticated, (req, res) => {
     })
 })
 
+router.post('/request', rejectUnauthenticated, (req, res) => {
+    console.log(req.body);
+    pool.query(`INSERT INTO "connection_request" ("from_person_id", "to_person_id")
+                VALUES ($1, $2);`, [req.user.id, req.body.id])
+    .then((results) => {
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('Error in connections post: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.get('/request/received', rejectUnauthenticated, (req, res) => {
+    pool.query(`SELECT * FROM "connection_request"
+    JOIN "person" ON "person"."id"="connection_request"."from_person_id"
+    WHERE "connection_request"."to_person_id"=$1;`, [req.user.id])
+    .then( (results) => {
+        res.send(results.rows);
+    })
+    .catch( (error) => {
+        console.log('Error in connection_request get: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.get('/request/sent', rejectUnauthenticated, (req, res) => {
+    pool.query(`SELECT * FROM "connection_request"
+    JOIN "person" ON "person"."id"="connection_request"."to_person_id"
+    WHERE "connection_request"."from_person_id"=$1;`, [req.user.id])
+    .then( (results) => {
+        res.send(results.rows);
+    })
+    .catch( (error) => {
+        console.log('Error in connection_request get: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.delete('/request/sent', rejectUnauthenticated, (req, res) => {
+    pool.query()
+    .then( (results) => {
+        res.sendStatus(200);
+    })
+    .catch( (error) => {
+        console.log('Error in connections delete: ', error);
+        res.sendStatus(500);
+    })
+})
+
+router.delete('/request/received', rejectUnauthenticated, (req, res) => {
+    pool.query()
+    .then( (results) => {
+        res.sendStatus(200);
+    })
+    .catch( (error) => {
+        console.log('Error in connections delete: ', error);
+        res.sendStatus(500);
+    })
+})
+
 module.exports = router;
