0d073ba532a0e004593a8e852022bf4ecb6763ea
Jake Marzitelli <mjakehs@gmail.com>
2018-09-29 20:15:03 -0500

    pop_up container now outside of tempalte views

--- server/public/scripts/project.controller.js
+++ server/public/scripts/project.controller.js
@@ -67,6 +67,54 @@ app.controller('ProjectController', ['$http', '$mdDialog', function ($http, $mdD
             console.log('Add alert here?')
           });
     }
+    vm.manageTeam = function (entry) {
+        $mdDialog.show({
+            parent: document.getElementById('popupContainer'),
+            targetEvent: event.currentTarget,
+            template:
+                `
+                <md-dialog ng-controller="EntryController as vm">
+                    <md-dialog-content>
+                    <h3>Edit Entry<h3>
+                    <div layout="row" layout-padding">
+                         <md-input-container class="entryInput">
+                            <label>Entry</label>
+                            <input type="text" ng-model="entryEdit.entry" placeholder="What did you do?" />
+                        </md-input-container>
+                         <md-input-container class="entryInput">
+                            <label>Project</label>
+                             <md-select ng-model="entryEdit.project_id">
+                                <md-option value="{{project.id}}" ng-repeat="project in vm.projectsList">{{project.name}}</option>
+                            </md-select>
+                        </md-input-container>
+                        <md-input-container class="entryInput">
+                            <label>Entry Date</label>
+                            <md-datepicker ng-model="entryEdit.date"></md-datepicker>
+                        </md-input-container>
+                        <md-input-container class="entryInput">
+                            <label>Hours</label>
+                            <input type="text" ng-model="entryEdit.hours">
+                        </md-input-container>
+                    </div>
+                    </md-dialog-content>
+                    <md-dialog-actions>
+                        <md-button ng-click="closeDialog()" class="md-warn md-raised">
+                            Cancel
+                        </md-button>
+                        <md-button ng-click="saveEdit(entryEdit.id)" class="md-primary md-raised">
+                            Save Edit
+                        </md-button>
+                    </md-dialog-actions>
+                </md-dialog>
+                `,
+            controller: DialogController
+        });
+        function DialogController($mdDialog, $scope, $http) {
+            $scope.closeDialog = function () {
+                $mdDialog.hide();
+            }
+        }
 
+    }
     vm.getProjects();
 }]);
