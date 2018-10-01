4873520f1077b0e8fcb4ad8e4a4c21010034f018
Jake Marzitelli <mjakehs@gmail.com>
2018-10-01 09:17:49 -0500

    update sql file

--- database.sql
+++ database.sql
@@ -4,12 +4,14 @@ CREATE TABLE "entry" (
     "id" SERIAL PRIMARY KEY,
     "entry" VARCHAR(255) NOT NULL,
     "project_id" INT REFERENCES "project", 
+    "person_id" INT REFERENCES "person",
     "entry_date" DATE NOT NULL,
     "hours" DECIMAL(4, 2) NOT NULL
 );
 
 CREATE TABLE "project" (
     "id" SERIAL PRIMARY KEY,
+    "person_id" INT REFERENCES "person",
     "name" VARCHAR(120) NOT NULL
 );
 
@@ -17,4 +19,4 @@ CREATE TABLE "person" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
-);
+);  
